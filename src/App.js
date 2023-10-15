1812fe17b2fe3f8e17320006540d4a2d38a84606
madhumitaamaharaj <madhumitaamaharaj7@gmail.com>
2023-10-15 22:41:41 +0530

    FEAT:Redux for State Management

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
+import { Provider } from 'react-redux';
 import Head from './components/Head/Head';
 import Body from './components/Body/Body';
+import store from './utils/store';
+
 function App() {
   return (
-    <div className="App">
-    
+    <Provider store={store}>
+    <div >
      <Head/>
      <Body/>
     </div>
+    </Provider>
+
   );
 }
 
